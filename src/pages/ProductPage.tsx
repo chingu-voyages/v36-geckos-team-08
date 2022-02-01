6aa95035524a61d7bc27a490a84bd0c19df44fa3
Cristhian <cristhiantilleria@gmail.com>
2022-02-01 12:35:19 -0500

    feat: product page implementation with commerce.js

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,43 +1,52 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import commerce from '../api';
 
-const product = {
-  name: 'Unicorn Blend',
-  price: 14,
-  image:
-    'https://cdn.shopify.com/s/files/1/0424/8862/7355/products/CT_PRODUCTS_SOCIAL_DOG_000-SRP-C.jpg?v=1634327763',
-  description:
-    ' light-to-medium roast with bright acidity and smoothly balanced flavors. Our most popular blend to flavor your morning.',
-  roast: 'dark'
-};
-
-export const ProductPage = (props: any) => {
+export const ProductPage = () => {
+  const [product, setProduct] = React.useState<any>({});
+  const [isLoading, setIsLoading] = React.useState(true);
   const { id } = useParams();
+  const productId = id as string;
+
+  React.useEffect(() => {
+    commerce.products.retrieve(productId).then((product) => {
+      setIsLoading(false);
+      setProduct(product);
+    });
+  }, [productId]);
+
+  const cleanDescription = product.description?.replace(/( |<([^>]+)>)/gi, ' ');
 
   const handleAddToCart = () => {
     //add to cart
   };
-
+  if (isLoading)
+    return (
+      <span className="h-screen flex w-full justify-center mt-20">
+        isLoading
+      </span>
+    );
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="bg-gray-100 py-10 h-4/5 w-full">
         <div className="flex flex-col sm:flex-row h-full max-w-6xl items-center py-20 justify-center md:justify-between mx-auto sm:px-12 lg:px-24">
-          <div className="mb-10 w-8/12 lg:w-10/12">
+          <div className="mb-10 w-8/12 lg:w-10/12 rounded-lg overflow-auto">
             <img
-              src={product.image || 'http://tny.im/rbC'}
+              src={product.image?.url || 'http://tny.im/rbC'}
               alt=""
-              className="w-full"
+              className="w-full "
             />
           </div>
           <section className="w-9/12 sm:ml-[10%]">
             <div className="mb-4 md:mb-10">
               <p className="font-bold text-lg mb-3">
-                {product.name || 'cofffe-name'}
+                {product.name || 'coffee-name'}
               </p>
-              <p className="uppercase mb-3 text-sm">$ price/bag size</p>
+              <p className="uppercase mb-3 text-sm">
+                {product.price?.formatted_with_symbol || '$ Price'}
+              </p>
               <p className="text-sm">
-                {product.description ||
-                  'Excepteur sint occaecat cupidatat non proident'}
+                {cleanDescription || 'description here'}
               </p>
             </div>
             <div className="w-full flex justify-center pr-5">
