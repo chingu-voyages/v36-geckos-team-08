11aef39534c9223a8b7854c3a013f5ee9493358d
Cristhian <cristhiantilleria@gmail.com>
2022-02-05 20:13:33 -0500

    fix: environment variable clean

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import commerce from '../api/commerce';
 
 export const ShopPage = () => {
-  const [listOfProducts, setListOfProducts] = React.useState<any>({});
-  const [isLoading, setIsLoading] = React.useState(true);
-  const getProducts = async () => {
-    try {
-      const response = await commerce.products.list();
-      setIsLoading(false);
-      setListOfProducts(response.data);
-    } catch (error) {
-      console.log('There was an error getting the product information', error);
-    }
-  };
-  getProducts();
   return <div className="h-screen flex flex-col "></div>;
 };
